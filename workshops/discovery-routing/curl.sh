eda4aaed302bce00e5056325a95a9a7164ac2421
Michael Bell <mbell@opentable.com>
2020-09-30 17:14:13 -0700

    More stuff

---
+++ workshops/discovery-routing/curl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+name=$(whoami)
+echo "launching curl for you ${name} - note this only works for admins on normal clusters"
+
+namespace=ws-${name}
+namespace=infra
+kubectl -n ${namespace} run -i --tty curl --image=docker.otenv.com/k8s-training:curl --restart=Never --command -- sh && kubectl -n ${namespace} delete pod curl
