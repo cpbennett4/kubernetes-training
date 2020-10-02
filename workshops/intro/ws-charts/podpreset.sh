4bd7733fe5d276c4bd1fe6038e8074f79a8cff29
Michael Bell <mbell@opentable.com>
2020-10-02 14:21:43 -0700

    podpreset backup

---
+++ workshops/intro/ws-charts/podpreset.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#set -o nounset -o errexit
+if ! [[ -x "$(command -v helm)" ]]; then
+	echo "Install helm please" 1>&2
+	exit 1
+fi
+
+if ! [[ -x "$(command -v kubectl)" ]]; then
+	echo "Install kubectl please" 1>&2
+	exit 1
+fi
+
+name=$(whoami)
+if [[ -z ${name} ]]; then
+  echo "No name provided - whoami didnt work."  1>&2
+  exit 1
+fi
+
+release=ot-env
+chart=cluster-settings
+namespace=ws-$name
+if [[ ! -d "ws-charts/$chart" ]]; then
+  echo "chart file ws-charts/$chart doesn't exist " 1>&2
+  exit 1
+fi
+# validate all 3 and existence of values and chart
+echo rendering  pod preset using: releasename=$release ...... chart=$chart.. namespace=$namespace 1>&2
+helm template $release ws-charts/$chart --set namespace=$namespace --set kind=test --set env=ci --set region=rs
+
